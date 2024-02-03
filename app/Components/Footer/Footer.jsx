212402411981ec714f18a2984f8d5deeb4d2c433
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-02-03 22:33:45 +0300

    add: modules

--- app/Components/Footer/Footer.jsx
+++ app/Components/Footer/Footer.jsx
@@ -1,23 +1,25 @@
+import Styles from './Footer.module.css';
+
 export const Footer = () => {
 	return (
-		<footer className='footer'>
-			<a href='./index.html' className='footer__logo'>
-				<span className='footer__logo-name'>pindie</span>
-				<span className='footer__logo-copy'>, XXI век</span>
+		<footer className={Styles.footer}>
+			<a href='./index.html' className={Styles.logo}>
+				<span className={Styles['logo - name']}>pindie</span>
+				<span className={Styles['logo-copy']}>, XXI век</span>
 			</a>
-			<ul className='social-list'>
-				<li className='social-list__item'>
-					<a href='' className='button social-list__link'>
+			<ul className={Styles['social-list']}>
+				<li className={Styles['social-list__item']}>
+					<a href='' className={Styles[('button', 'social-list__link')]}>
 						YT
 					</a>
 				</li>
-				<li className='social-list__item'>
-					<a href='' className='button social-list__link'>
+				<li className={Styles['social-list__item']}>
+					<a href='' className={Styles[('button', 'social-list__link')]}>
 						ВК
 					</a>
 				</li>
-				<li className='social-list__item'>
-					<a href='' className='button social-list__link'>
+				<li className={Styles['social-list__item']}>
+					<a href='' className={Styles[('button', 'social-list__link')]}>
 						TG
 					</a>
 				</li>
