f8bdc0f6c19731f5bbd1d0e919268c4e58382b4a
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-02-03 23:33:38 +0300

    add: promo styles

--- app/Components/Promo/Promo.jsx
+++ app/Components/Promo/Promo.jsx
@@ -1,12 +1,14 @@
+import Styles from './Promo.module.css';
+
 export const Promo = () => {
-    return (
-			<section class='promo'>
-				<div class='promo__description-block'>
-					<h2 class='promo__title'>Твой промо-код</h2>
-					<p class='promo__description'>Скидка на все курсы Яндекс Практикума для пользователей нашего сайта!</p>
-					<button class='button promo__button'>Получить код</button>
-				</div>
-				<img src='./images/promo-illustration.svg' alt='Собака' class='promo__image' />
-			</section>
-		);
-}
+	return (
+		<section className={Styles.promo}>
+			<div className={Styles['description-block']}>
+				<h2 className={Styles.title}>Твой промо-код</h2>
+				<p className={Styles.description}>Скидка на все курсы Яндекс Практикума для пользователей нашего сайта!</p>
+				<button className={`${Styles.button} ${Styles.promo__button}`}>Получить код</button>
+			</div>
+			<img src='./images/promo-illustration.svg' alt='Собака' className={Styles.image} />
+		</section>
+	);
+};
