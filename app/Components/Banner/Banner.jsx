36d39acb2073b7a03725d6afd0f2598fe455b132
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-02-03 22:47:52 +0300

    add: banner module styles

--- app/Components/Banner/Banner.jsx
+++ app/Components/Banner/Banner.jsx
@@ -1,19 +1,21 @@
+import Styles from './Banner.module.css';
+
 export const Banner = () => {
-    return (
-			<section class='banner'>
-				<div class='banner__description'>
-					<h1 class='banner__title'>Портал инди-игр от&nbsp;студентов Яндекс Практикума</h1>
-					<div class='banner__text-block'>
-						<p class='banner__text'>
-							Студенты курсов разрабатывают свои игры на Unity, здесь мы их публикуем. Вы можете играть прямо на сайте.
-							А если у вас есть аккаунт пользователя — получаете возможность голосовать за игры.
-						</p>
-					</div>
-					<a href='#popular' class='button banner__link'>
-						Смотреть игры
-					</a>
+	return (
+		<section className={Styles.banner}>
+			<div className={Styles['banner__description']}>
+				<h1 className={Styles.title}>Портал инди-игр от&nbsp;студентов Яндекс Практикума</h1>
+				<div className={Styles['text-block']}>
+					<p className={Styles.text}>
+						Студенты курсов разрабатывают свои игры на Unity, здесь мы их публикуем. Вы можете играть прямо на сайте. А
+						если у вас есть аккаунт пользователя — получаете возможность голосовать за игры.
+					</p>
 				</div>
-				<img src='./images/banner-illustration.jpg' alt='Рука, утопленная в желтом фоне' class='banner__image' />
-			</section>
-		);
-}
+				<a href='#popular' className={`${Styles.button} ${Styles.link}`}>
+					Смотреть игры
+				</a>
+			</div>
+			<img src='./images/banner-illustration.jpg' alt='Рука, утопленная в желтом фоне' className={Styles.image} />
+		</section>
+	);
+};
