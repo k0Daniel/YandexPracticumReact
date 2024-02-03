5cbf81ddb1ac7e1708f0f346f8e234cbb512f37a
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-02-03 13:40:46 +0300

    fix: CardsList folder name

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { Banner } from "./Components/Banner/Banner";
-import { NewCardsList } from "./Components/CardList/NewCardsList";
-import { PopularCardsList } from "./Components/CardList/PopularCardsList";
+import { NewCardsList } from "./Components/CardsList/NewCardsList";
+import { PopularCardsList } from "./Components/CardsList/PopularCardsList";
 import { Promo } from "./Components/Promo/Promo";
 
 export default function Home() {
