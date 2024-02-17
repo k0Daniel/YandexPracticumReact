be86cca698785ce523b4097db0167437d3268588
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-02-17 13:03:49 +0300

    add: single data source

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
+import { getGamesByCategory } from './data/data-utils';
 import { Banner } from './Components/Banner/Banner';
 import { CardsList } from './Components/CardsList/CardsList';
-import { NewCardsFragment } from './Components/CardsList/NewCardsFragment';
-import { PopularCardsFragment } from './Components/CardsList/PopularCardsFragment';
 import { Promo } from './Components/Promo/Promo';
 
 export default function Home() {
+	const popularGames = getGamesByCategory('popular');
+	const newGames = getGamesByCategory('new');
 	return (
 		<main>
 			<Banner />
-			<CardsList id='popular' title='Популярные'></CardsList>
-			<CardsList id='new' title='Новинки'></CardsList>
+			<CardsList id='popular' title='Популярные' data={popularGames} />
+			<CardsList id='new' title='Новинки' data={newGames} />
 			<Promo />
 		</main>
 	);
