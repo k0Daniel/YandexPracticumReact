a0e5355b9f197093b4e490a5769198488b1532e3
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-03-17 17:02:11 +0300

    fix: uncorrect "Components" calling

--- app/new/page.js
+++ app/new/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useGetDataByCategory } from '../api/api-hooks';
-import { CardsListSection } from '../components/CardsListSection/CardsListSection';
+import { CardsListSection } from '../Components/CardsListSection/CardsListSection';
 import { endpoints } from '../api/config';
 import { Preloader } from '../Components/Preloader/Preloader';
 
@@ -8,7 +8,7 @@ export default function newGames() {
 	const newGames = useGetDataByCategory(endpoints.games, 'new');
 	return (
 		<main className='main-inner'>
-			{newGames ? <CardsListSection type="slider" id='new' title='Новые' data={newGames} /> : <Preloader />}
+			{newGames ? <CardsListSection type='slider' id='new' title='Новые' data={newGames} /> : <Preloader />}
 		</main>
 	);
 }
--- app/shooters/page.js
+++ app/shooters/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useGetDataByCategory } from '../api/api-hooks';
-import { CardsListSection } from '../components/CardsListSection/CardsListSection';
+import { CardsListSection } from '../Components/CardsListSection/CardsListSection';
 import { endpoints } from '../api/config';
 import { Preloader } from '../Components/Preloader/Preloader';
 
